7a40f19d3de6123b6130dc32bf85da3acda8a57b
Stiney <stinebiser1@hotmail.com>
2016-01-21 22:21:25 -0500

    Draft of assignment1

---
+++ c101/stiney/assignment1.rb
@@ -0,0 +1,3 @@
+puts "I got your float and integer right here (crotch grab)...#{(2.0+3.0)} and #{(7+1)}"
+puts "The number of days between June 18, 1982 and January 29, 2016 is...#{((33*365)+8)+((7*30)+4)+(7+4)}" 
+puts "The number of days that are in months with less than 31 days over a 4 year period...#{((5*30)*4)+1}" 
