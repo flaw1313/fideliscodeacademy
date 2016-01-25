8e8a19e72d58b50ff9351c8e3ef4f695fc1ec8eb
Frank Law <flaw@arrowagent.com>
2016-01-24 23:57:25 -0500

    Assignment 1 done

---
+++ c101/frank/assignment1.rb
@@ -0,0 +1,16 @@
+# task #1
+puts 'Here is the result of the float 5.0 and the integer 8:'
+puts 5.0 + 8
+puts
+
+# task #2
+puts 'I will be this many days old by the due date: '
+# equation is (Number of days in a year * my age) + (one day for every leap year) 
+# + the number of days from my last birthday
+puts (365 * 39) + (9 * 1) + 14
+puts
+
+# task #3
+puts 'The number of days in the less than 31 day months over the past 4 years:'
+# equation is 
+puts (((4 * 30) + 28) * 4) + 1
