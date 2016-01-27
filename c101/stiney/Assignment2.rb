b9235b23e5e946e54c69c86287e166c015e4871a
Stiney <stinebiser1@hotmail.com>
2016-01-26 22:15:17 -0500

    Assignment 2 revised

--- c101/stiney/Assignment2.rb
+++ c101/stiney/Assignment2.rb
@@ -4,4 +4,4 @@ puts "Frank Rocks " *100
 this_class_var1 = 'This class pretty okay so far'
 this_class_var2 = 'You damn skiznippy'
 this_class_var3 = 'Fuckin\' A'
-puts this_class_var1 + '\\' + this_class_var2 + '\\' + this_class_var3
+puts this_class_var1 + '\\' + this_class_var2 + '\\' + '\\' + this_class_var3
