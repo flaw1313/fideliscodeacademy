78014a1c8e48efafaba3c818d371ecbbe4307aff
michaeldavidg <mgurlides@gmail.com>
2016-01-28 22:56:59 -0500

    revision of assignment 1 with added comments

--- c101/mike/assignment1.rb
+++ c101/mike/assignment1.rb
@@ -1,4 +1,7 @@
 puts 2.0 + 3.0
 puts 5 + 2 + 1
-puts (34 * 365) + 8 + 74
+#my years x days in a year + leap years.  then subtract 30 days in my birth month of November from
+#17, my birthday.  + 31 days in December, + 29 days in January
+puts (34 * 365) + 8 + (30-17) + 31 + 29
+#4 years x February days + one leap year day + 4 years x 30 days x how many months with 30 days
 puts (4 * 28) + 1 + (4 * 30) *4
