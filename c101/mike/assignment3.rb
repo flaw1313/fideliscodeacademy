02e6c5cffa64d0ba25e9a25e71bbef6b9f48c879
michaeldavidg <mgurlides@gmail.com>
2016-02-04 10:43:04 -0500

    assignment 4 completed

---
+++ c101/mike/assignment3.rb
@@ -0,0 +1,14 @@
+#Creating a variable and storing the integer of 13 below, for lines 2 and 3
+var1 = 13
+puts var1 * 1
+#Creating a variable and storing the integer of 9 below, lines 5 and 6
+var2 = 9
+puts var2 * 1 
+#adding the 2 variables together on line 8
+puts var1 + var2
+#name variables below
+name = 'Mike '
+puts name
+name2 = 'Gurlides '
+puts name2
+puts name + name2 + 'is neat.'
---
+++ c101/mike/example1.rb
@@ -0,0 +1,9 @@
+puts gets
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name + '?  What a lovely name!'
+puts 'Pleased to meet you, ' + '.  :)'
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '.  :)'
+
