59fec9acc5c158b500282207843088c92feb4759
lstavale135 <lstavale@gmail.com>
2016-02-05 10:17:26 -0500

    Create Assignment4.rb
    Creating Assignment 4 via github and repl.it :)

---
+++ c101/lou/Assignment4.rb
@@ -0,0 +1,43 @@
+#Assignment 4
+#
+#Create a program in your project directory called assignment4.rb
+#Store the string '23' in a variable, then convert it to an integer and add it to the number 10, print out the result
+#Prompt the user to enter their name, and using their input, print out "Hello [ENTERED FIRST NAME], I hope you are having a wonderful day"
+#Prompt the user for 2 numbers, show the results of adding, subtracting, multiplying, and dividing the two numbers.
+#
+puts '___________________________________________________________________________________________________________________________________'
+puts ' '
+#
+
+#Task 1: 
+#Store the string '23' in a variable, then convert it to an integer and add it to the number 10, print out the result
+#
+puts 'Task 1: '
+puts 'Store the string \'23\' in a variable, then convert it to an integer and add it to the number 10, print out the result'
+#variables:
+#first_we_had_21_jumpstreet_then_22_jumpstret_guess_what's_next = '23' + ' jumpstreet!'
+puts ' '
+puts 'My variable is: '
+puts 'first_we_had_21_jumpstreet_then_22_jumpstret_guess_what\'s_next = \'23\'' + ' jumpstreet!'
+#actual variable will be cleaned up
+first_we_had_21_jumpstreet_then_22_jumpstret_guess_whats_next = '23'
+#Now convert to an integer and add to 10
+#
+puts ' '
+#to make this work as a single line, will convert .to_i, but then convert .to_s
+puts 'Now a decade after \'23 Jumpstreet\', Jonah Hill is old and saggy, but hey we have ' + '\'' + (first_we_had_21_jumpstreet_then_22_jumpstret_guess_whats_next.to_i + 10).to_s + ' Jumpstreet\'!'
+puts ' '
+puts 'Sincerely,'
+puts 'Korean Jesus'
+puts ' '
+puts 'PS:  Hey! stop fucking with Korean Jesus! He ain\'t got time for your problems! He\'s busy... with Korean shit!'
+puts ' '
+puts '_____________________________________________________________________________________________________________________________________________'
+#
+
+#Task 2:
+#Prompt the user to enter their name, and using their input, print out "Hello [ENTERED FIRST NAME], I hope you are having a wonderful day"
+#
+puts 'Task 2: '
+puts 'Prompt the user to enter their name, and using their input, print out "Hello [ENTERED FIRST NAME], I hope you are having a wonderful day'
+puts ' '
