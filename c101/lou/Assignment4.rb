dcaea91e725f3590222ba4f726cce340a4341756
lstavale135 <lstavale@gmail.com>
2016-02-05 11:03:41 -0500

    Update Assignment4.rb
    Working on figuring arithmetic with the gets function...

--- c101/lou/Assignment4.rb
+++ c101/lou/Assignment4.rb
@@ -5,7 +5,7 @@
 #Prompt the user to enter their name, and using their input, print out "Hello [ENTERED FIRST NAME], I hope you are having a wonderful day"
 #Prompt the user for 2 numbers, show the results of adding, subtracting, multiplying, and dividing the two numbers.
 #
-puts '___________________________________________________________________________________________________________________________________'
+puts '____________________________________________________________________________________________________________________________________________'
 puts ' '
 #
 
@@ -33,6 +33,7 @@ puts ' '
 puts 'PS:  Hey! stop fucking with Korean Jesus! He ain\'t got time for your problems! He\'s busy... with Korean shit!'
 puts ' '
 puts '_____________________________________________________________________________________________________________________________________________'
+puts ' '
 #
 
 #Task 2:
@@ -41,3 +42,52 @@ puts '__________________________________________________________________________
 puts 'Task 2: '
 puts 'Prompt the user to enter their name, and using their input, print out "Hello [ENTERED FIRST NAME], I hope you are having a wonderful day'
 puts ' '
+#Crazy Valentine program, haha :)
+#
+puts 'Hello there, I\'m Ruby.  So, what\'s your name by the way?'
+puts 'Can you please do Last Name first, followed by your First Name, and then complete it with your middle name; I\'m a learning program, so I do greatly appreciate it!'
+puts ' '
+Last_name = gets.chomp
+First_name = gets.chomp
+Middle_name = gets.chomp
+puts ' '
+puts '"Hello ' + First_name + ', I hope you\'re having a wonderful day"' +'!'
+puts ' '
+puts 'Until next time ' + First_name + ' '+ Middle_name + ' ' + Last_name
+puts ' '
+puts '<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3'
+puts ' '
+puts 'I\'ll find you, I\'m everywhere!'
+puts ' '
+puts '<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3'
+puts ' '
+puts '_____________________________________________________________________________________________________________________________________________'
+puts ' '
+#
+
+#Task 3:
+#Prompt the user for 2 numbers, show the results of adding, subtracting, multiplying, and dividing the two numbers.
+#
+puts 'Task 3: '
+puts 'Prompt the user for 2 numbers, show the results of adding, subtracting, multiplying, and dividing the two numbers.'
+puts ' '
+#Crazy Ruby continued
+#
+puts 'Hi ' + First_name + ', I told you I\'d find you! '  + ' Now if you want me to go away, I\'ll need you to enter two numbers for me please.'
+puts ' '
+puts 'Please enter your two numbers below: '
+puts ' '
+Numero_uno = gets.chomp
+Dueces_wild = gets.chomp
+puts ' '
+#adding
+#
+puts + First_name + ', it\'s been '
+puts + (Numero_uno + Dueces_wild).to_s + ' days since you last talked to me!' 
+#subtracting
+#
+#multiplying
+#
+#dividing
+
+
