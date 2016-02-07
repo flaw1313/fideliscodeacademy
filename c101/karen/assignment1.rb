f8749b11d7f1da556aa3b1c7fa2c80281771e30a
Karen Law <karenlaw13@gmail.com>
2016-02-07 14:29:31 -0500

    finished assignments 3 and 4

--- c101/karen/assignment1.rb
+++ c101/karen/assignment1.rb
@@ -2,7 +2,45 @@ puts 5.0 + 8
 puts (41 * 365) + 117 + 10
 puts (4 * 30) * 4 + (28 * 3) + 29 
 
+puts gets
+puts 'Hello there, what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+
+my_variable = 13
+puts my_variable 
+
+my_variable = 80
+my_variable = my_variable + 80
+puts my_variable 
+
+puts 13.0.to_f
+
+puts 'Today is 420 day'.to_f
+
+my_value = gets
+puts my_value
+
+my_new_value=gets.chomp
+puts my_new_value
+
+# by doing this i see program doesn't
+# print adds to the line and does not have a carriage retun and puts automatically adds a return 
+# stackoverflow is a good website for info 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+name = gets.chomp
+puts name
 
---
+++ c101/karen/assignment3.rb
@@ -0,0 +1,30 @@
+frank_fav = 13
+karen_fav = 9
+#our favorite numbers are name_fav variable 
+puts frank_fav + karen_fav
+my_name = 'Karen'
+my_lastname = 'Law'
+#first and last name my_name 
+puts my_name + ' ' + my_lastname
+rate = 3.75/1200
+present_value = 233000
+number_periods = 30 * 12
+top_line = rate * present_value
+bottom_line = 1 - (1 + rate)**-number_periods
+payment = top_line / bottom_line
+puts payment
+rate = 3.75/1200
+present_value = 233000
+number_periods = 30 * 12 
+top_line = rate * present_value
+bottom_line = 1 - (1 + rate)**-number_periods
+payment - top_line / bottom_line
+puts payment 
+
+
+
+
+
+
+
+
